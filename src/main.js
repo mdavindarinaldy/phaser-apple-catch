cb78954e88ee0de8a242a8f72ef6ea7865c4927e
Naldy <mdavindarinaldy@gmail.com>
2025-07-29 17:56:40 +0700

    feat: add target (apple) to spawn randomly

--- src/main.js
+++ src/main.js
@@ -14,20 +14,36 @@ class GameScene extends Phaser.Scene{
     this.player
     this.cursor
     this.playerSpeed=speedDown+50
+    this.target
+    this.points = 0
   }
   preload(){
     this.load.image("bg","/assets/bg.png")
     this.load.image("basket","/assets/basket.png")
+    this.load.image("apple","/assets/apple.png")
   }
   create(){
     this.add.image(0,0,"bg").setOrigin(0,0)
     this.player = this.physics.add.image(0,sizes.height-100,"basket").setOrigin(0,0)
     this.player.setImmovable(true)
     this.player.body.allowGravity = false
+    this.player.setCollideWorldBounds(true)
+    // this.player.setSize(80,15).setOffset(10,70)
+    this.player.setSize(this.player.width-this.player.width/4, this.player.height/6).setOffset(this.player.width/10, this.player.height-this.player.height/10)
+
+    this.target = this.physics.add.image(0, 0, "apple").setOrigin(0,0)
+    this.target.setMaxVelocity(0, speedDown)
+
+    this.physics.add.overlap(this.target, this.player, this.targetHit, null, this)
 
     this.cursor=this.input.keyboard.createCursorKeys()
   }
   update(){
+    if (this.target.y >= sizes.height) {
+      this.target.setY(0)
+      this.target.setX(this.getRandomX())
+    }
+
     const {left, right}= this.cursor
     if (left.isDown) {
       this.player.setVelocityX(-this.playerSpeed)
@@ -36,6 +52,16 @@ class GameScene extends Phaser.Scene{
     } else {
       this.player.setVelocityX(0)
     }
+  }
+
+  getRandomX() {
+    return Math.floor(Math.random()*400)
+  }
+  
+  targetHit() {
+    this.target.setY(0)
+    this.target.setX(this.getRandomX())
+    this.points++
   }
 }
 
